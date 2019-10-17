3051f5fcbf7e737138b4929bd094eb839d12854b
ljdsw <872176556@qq.com>
2019-10-17 11:28:27 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,6 +1,16 @@
 //9x9空表
+str = '<table border="1">'
+for(let i=1;i<=9;i++){
+    str += '<tr>'
+    for(var j=1;j<=9;j++){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str 
 //倒九九乘法表空表
-let str = '<table border="1">'
+str = '<table border="1">'
 for(let i=9;i>0;--i){
     str += '<tr>'
     for(var j=i;j>0;--j){
@@ -11,3 +21,13 @@ for(let i=9;i>0;--i){
 str += '</table>'
 document.getElementById('table2').innerHTML = str 
 //倒九九乘法空表
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>' + i + '*' + j + '=' + (i * j) + '</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str 
